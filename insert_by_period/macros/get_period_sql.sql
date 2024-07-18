e07651234b67bd5e8917ac5d8e818d16ad84630c
Aliaksei Adamovich <15641232+AdamovichAleksey@users.noreply.github.com>
2024-07-17 21:33:31 -0400

    added teradata support to get_period_sql.sql

--- insert_by_period/macros/get_period_sql.sql
+++ insert_by_period/macros/get_period_sql.sql
@@ -25,33 +25,38 @@
 {% macro teradata__get_period_sql(target_cols_csv, sql, timestamp_field, period, start_timestamp, stop_timestamp, offset) -%}
 
   {%- if period == 'day' -%}
-    {%- set interval_expression = 'INTERVAL \'' ~ offset ~ '\' DAY' -%}
+    {%- set offset_expression = 'interval \'' ~ offset ~ '\' day' -%}
   {%- elif period == 'week' -%}
-    {%- set interval_expression = 'INTERVAL \'' ~ (offset * 7) ~ '\' DAY' -%}
+    {%- set offset_expression = 'interval \'' ~ ((offset | int) * 7) ~ '\' day' -%}
   {%- elif period == 'month' -%}
-    {%- set interval_expression = 'INTERVAL \'' ~ offset ~ '\' MONTH' -%}
+    {%- set offset_expression = 'interval \'' ~ offset ~ '\' month' -%}
+  {%- endif %}
+
+  {%- if period == 'day' -%}
+    {%- set interval_expression = "interval '1' day" -%}
+  {%- elif period == 'week' -%}
+    {%- set interval_expression = "interval '7' day" -%}
+  {%- elif period == 'month' -%}
+    {%- set interval_expression = "interval '1' month" -%}
   {%- endif %}
 
   {%- set period_filter -%}
     (
-      {{ timestamp_field }} > CAST('{{ start_timestamp }}' AS TIMESTAMP(0)) + {{ interval_expression }} AND
-      {{ timestamp_field }} <= CAST('{{ start_timestamp }}' AS TIMESTAMP(0)) + {{ interval_expression }} + 
-        CASE '{{ period }}'
-          WHEN 'day' THEN INTERVAL '1' DAY
-          WHEN 'week' THEN INTERVAL '7' DAYS
-          WHEN 'month' THEN INTERVAL '1' MONTH
-        END AND
-      {{ timestamp_field }} < CAST('{{ stop_timestamp }}' AS TIMESTAMP(0))
+      {{ timestamp_field }} >= cast('{{ start_timestamp }}' as timestamp(0)) + {{ offset_expression }} 
+      and {{ timestamp_field }} < least(
+        cast('{{ start_timestamp }}' as timestamp(0)) + {{ offset_expression }} + {{ interval_expression }},
+        cast('{{ stop_timestamp }}' as timestamp(0))
+      )
     )
   {%- endset -%}
 
   {%- set filtered_sql = sql | replace("__PERIOD_FILTER__", period_filter) -%}
 
-  SELECT
+  select
     {{target_cols_csv}}
-  FROM (
+  from (
     {{filtered_sql}}
-  ) AS target_cols
+  ) as target_cols
 
 {%- endmacro %}
 
