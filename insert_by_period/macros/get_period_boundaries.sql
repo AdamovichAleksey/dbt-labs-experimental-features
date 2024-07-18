3244ea072e6a68c7f6f405351ff36902292a027f
Aliaksei Adamovich <15641232+AdamovichAleksey@users.noreply.github.com>
2024-07-17 21:32:56 -0400

    added teradata support to get_period_boundaries.sql

--- insert_by_period/macros/get_period_boundaries.sql
+++ insert_by_period/macros/get_period_boundaries.sql
@@ -1,8 +1,12 @@
-{% macro get_period_boundaries(target_schema, target_table, timestamp_field, start_date, stop_date, period, backfill, full_refresh_mode) -%}
-    {{ return(adapter.dispatch('get_period_boundaries', 'insert_by_period')(target_schema, target_table, timestamp_field, start_date, stop_date, period, backfill, full_refresh_mode)) }}
+{% macro get_period_boundaries(target_schema, target_table, timestamp_field, timestamp_field_target, start_date, stop_date, period, backfill, full_refresh_mode) -%}
+    {{ return(adapter.dispatch('get_period_boundaries', 'insert_by_period')(target_schema, target_table, timestamp_field, timestamp_field_target, start_date, stop_date, period, backfill, full_refresh_mode)) }}
 {% endmacro %}
 
-{% macro default__get_period_boundaries(target_schema, target_table, timestamp_field, start_date, stop_date, period, backfill, full_refresh_mode) -%}
+
+
+{% macro default__get_period_boundaries(target_schema, target_table, timestamp_field, timestamp_field_target, start_date, stop_date, period, backfill, full_refresh_mode) -%}
+
+  {%- set timestamp_field_target_col_name = timestamp_field_target.split('.')[-1] -%}
 
   {% call statement('period_boundaries', fetch_result=True) -%}
     with data as (
@@ -10,7 +14,7 @@
           {% if backfill and not full_refresh_mode -%}
             cast('{{start_date}}' as timestamp) as start_timestamp,
           {%- else -%}
-            coalesce(max({{timestamp_field}}), cast('{{start_date}}' as timestamp)) as start_timestamp,
+            coalesce(max({{ timestamp_field_target_col_name }}), cast('{{start_date}}' as timestamp)) as start_timestamp,
           {%- endif %}
           coalesce(
             {{ dateadd('millisecond',
@@ -34,35 +38,35 @@
 
 
 
-{% macro teradata__get_period_boundaries(target_schema, target_table, timestamp_field, start_date, stop_date, period, backfill, full_refresh_mode) -%}
+{% macro teradata__get_period_boundaries(target_schema, target_table, timestamp_field, timestamp_field_target, start_date, stop_date, period, backfill, full_refresh_mode) -%}
 
-  {% call statement('period_boundaries', fetch_result=True) -%}
-    WITH data AS (
-      SELECT
+  {%- set timestamp_field_target_col_name = timestamp_field_target.split('.')[-1] -%}
+
+  {% call statement('period_boundaries', fetch_result=true) -%}
+    with data as (
+      select
           {% if backfill and not full_refresh_mode -%}
-            CAST('{{ start_date }}' AS TIMESTAMP(0)) AS start_timestamp,
+            cast('{{ start_date }}' as timestamp(0)) as start_timestamp,
           {%- else -%}
-            COALESCE(MAX({{ timestamp_field }}), CAST('{{ start_date }}' AS TIMESTAMP(0))) AS start_timestamp,
+            coalesce(
+              cast(max({{ timestamp_field_target_col_name }}) as timestamp(0)), 
+              cast(date '{{ start_date }}' as timestamp(0))
+            ) as start_timestamp,
           {%- endif %}
-          COALESCE(
-            -- Subtract 1 second from stop_date if it is not null
-            CAST(NULLIF('{{ stop_date }}', '') AS TIMESTAMP(0)) - INTERVAL '0.001' SECOND,
-            -- Default to current_timestamp if stop_date is null or empty
-            CAST(CURRENT_TIMESTAMP AS TIMESTAMP(0))
-          ) AS stop_timestamp
-      FROM {{ adapter.quote(target_schema) }}.{{ adapter.quote(target_table) }}
+            cast(date '{{ stop_date }}' as timestamp(0)) - interval '0.001' second as stop_timestamp
+      from {{ adapter.quote(target_schema) }}.{{ adapter.quote(target_table) }}
     )
 
-    SELECT
+    select
       start_timestamp,
       stop_timestamp,
-      -- Calculate the difference in the specified period units between start and stop timestamps
-      CASE '{{ period }}'
-        WHEN 'day' THEN (stop_timestamp - start_timestamp ) DAY
-        WHEN 'week' THEN (stop_timestamp - start_timestamp ) DAY / 7
-        WHEN 'month' THEN (stop_timestamp - start_timestamp ) MONTH
-      END + 1 AS num_periods
-    FROM data
+      -- calculate the difference in the specified period units between start and stop timestamps
+      case 
+        when '{{ period }}' = 'day' then cast(((stop_timestamp - start_timestamp ) day) as int) + 1
+        when '{{ period }}' = 'week' then cast(((stop_timestamp - start_timestamp ) day) as int) / 7 + 1
+        when '{{ period }}' = 'month' then cast(((stop_timestamp - start_timestamp ) month) as int) + 1
+      end as num_periods
+    from data
   {%- endcall %}
 
 {%- endmacro %}
@@ -77,7 +81,7 @@
           {% if backfill and not full_refresh_mode -%}
           cast('{{start_date}}' as timestamp) as start_timestamp,
           {%- else -%}
-          coalesce(max({{timestamp_field}}), cast('{{start_date}}' as timestamp)) as start_timestamp,
+          coalesce(max({{ timestamp_field_col_name }}), cast('{{start_date}}' as timestamp)) as start_timestamp,
           {%- endif %}
           coalesce(datetime_add(cast(nullif('{{stop_date}}','') as timestamp), interval -1 millisecond), {{dbt.current_timestamp()}}) as stop_timestamp
       from {{adapter.quote(target_schema)}}.{{adapter.quote(target_table)}}
